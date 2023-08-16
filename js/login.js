e0e27c08a0918a5d6238e6cde540f685913c0899
noblemandres <noble16_2015@hotmail.com>
2023-08-15 22:01:30 -0300

    edit4

--- js/login.js
+++ js/login.js
@@ -1,10 +1,12 @@
-document.getElementById("btn").addEventListener("click", function() {
+document.getElementById("btn").addEventListener("click", function(event) {
+    event.preventDefault(); // Evita el envío automático del formulario
+
     var username = document.getElementById("emailinput").value;
     var password = document.getElementById("password").value;
 
-    if (username === ""  || password === "") {
-    alert("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
+    if (username === "" || password === "") {
+        alert("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
     } else {
-    location.replace("index.html");
+        location.replace("index.html");
     }
 });
